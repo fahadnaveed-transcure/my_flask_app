b056490f5d6bb8b3dfa2a5482398f27279224e84
fahadnaveed-transcure <fahad.naveed@transcure.biz>
2025-04-28 15:26:04 +0500

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,20 @@ app = Flask(__name__)
 @app.route('/')
 def home():
     #return "Hello, Flask!"
-    return render_template('index.html')
+    return render_template('imagetext.html')
+
+@app.route('/ImageDesc')
+def ImageDesc():
+    return render_template('imagedes.html')
+
+@app.route('/textsummary')
+def textsummary():
+    return render_template('textsummary.html')
+
+
+@app.route('/textimage')
+def textimage():
+    return render_template('textimage.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
-#this is a test comment 
